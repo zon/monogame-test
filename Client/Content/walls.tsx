24380659f3615dd9a66794d61c304a01cfd5f195
Matthew Z Haralovich <zon@haralovich.org>
2021-03-27 22:23:38 -0400

    Added Tiled map loading and rendering.

---
+++ Client/Content/walls.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="walls" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="walls.png" width="128" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="spawn" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ Client/Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+
+namespace MonoGameTest
+{
+	public class Game1 : Game
+	{
+		TiledMap _tiledMap;
+		TiledMapRenderer _tiledMapRenderer;
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			this.Window.Title = "MonoGame Test";
+
+			_graphics.PreferredBackBufferWidth = 800;
+			_graphics.PreferredBackBufferHeight = 600;
+			_graphics.ApplyChanges();
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			_tiledMap = Content.Load<TiledMap>("first");
+			_tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+			
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			_tiledMapRenderer.Update(gameTime);
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			_tiledMapRenderer.Draw();
+
+			base.Draw(gameTime);
+		}
+	}
+}
